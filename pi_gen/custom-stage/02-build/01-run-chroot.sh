12e55271178585b3a868398d06422f8a335fb2e6
Graham Stewart <grahamsz@gmail.com>
2024-01-21 10:39:16 -0700

    Initial Commit

---
+++ pi_gen/custom-stage/02-build/01-run-chroot.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+# This cleans up the boot config files
+
+#remove any auto detect lines
+sed -i '/camera_auto_detect/d' /boot/config.txt
+
+#disable camera autodetect and add overlays for both the camera ande display
+echo 'camera_auto_detect=0' >> /boot/config.txt
+echo 'dtoverlay=imx708'>> /boot/config.txt
+echo 'dtoverlay=gc9a01' >> /boot/config.txt
+echo 'dtparam=spi=on' >> /boot/config.txt
+
+# If necessary we can checkout and build specific things in another chroot file
